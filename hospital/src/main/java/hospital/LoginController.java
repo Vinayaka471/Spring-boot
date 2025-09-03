29e53c771ec946dd378a92e6743b0eb47e08a435
vinayaka <vinayakah47@gmail.com>
2025-09-03 07:44:41 +0530

    Login, Logout page using Spring web, Spring security and JSP

---
+++ hospital/src/main/java/hospital/LoginController.java
@@ -0,0 +1,18 @@
+package hospital;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/")
+	public String showLoginPage() {
+		return "index";
+	}
+
+	@GetMapping("/welcome")
+	public String showWelcome() {
+		return "welcome";
+	}
+}
---
+++ hospital/src/main/java/hospital/SecurityConfig.java
@@ -0,0 +1,28 @@
+package hospital;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		String[] allowedPaths = { "/", "/WEB-INF/views/**", "/static/**", "/css/**", "/js/**", "/images/**" };
+
+		httpSecurity.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(
+						authorize -> authorize.requestMatchers(allowedPaths).permitAll().anyRequest().authenticated())
+				.formLogin(form -> form.loginPage("/").loginProcessingUrl("/login").usernameParameter("userEmail")
+						.passwordParameter("userPassword").defaultSuccessUrl("/welcome", true).failureUrl("/?error")
+						.permitAll())
+				.logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/").invalidateHttpSession(true)
+						.clearAuthentication(true).permitAll());
+
+		return httpSecurity.build();
+	}
+}
